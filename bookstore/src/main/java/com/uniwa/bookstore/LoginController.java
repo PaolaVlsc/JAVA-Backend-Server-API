0b4ecf6ff68ce3136a08ed654b977aa40cf4974b
PaolaVlsc <paolavlsc170698@gmail.com>
2024-12-27 12:19:41 +0200

    add debug log

--- bookstore/src/main/java/com/uniwa/bookstore/LoginController.java
+++ bookstore/src/main/java/com/uniwa/bookstore/LoginController.java
@@ -7,20 +7,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/api")
 public class LoginController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody User user) {
+        logger.debug("Login request received for username: {}", user.getUsername());
         User foundUser = userRepository.findByUsername(user.getUsername());
         if (foundUser != null && foundUser.getPassword().equals(user.getPassword())) {
+            logger.debug("Login successful for username: {}", user.getUsername());
             return ResponseEntity.ok("Login successful");
         } else {
+            logger.debug("Invalid credentials for username: {}", user.getUsername());
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
         }
     }
--- bookstore/src/main/java/com/uniwa/bookstore/User.java
+++ bookstore/src/main/java/com/uniwa/bookstore/User.java
@@ -1,6 +1,5 @@
 package com.uniwa.bookstore;
 
-import jakarta.annotation.Generated;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
