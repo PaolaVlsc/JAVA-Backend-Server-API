6c361c965f140af220f3afcfc48876d7856ef035
PaolaVlsc <paolavlsc170698@gmail.com>
2024-12-28 15:16:17 +0200

    retrieve book by id

--- bookstore/src/main/java/com/uniwa/bookstore/BookController.java
+++ bookstore/src/main/java/com/uniwa/bookstore/BookController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -23,5 +24,29 @@ public class BookController {
         List<Book> books = bookRepository.findAll();
         logger.debug("Fetched {} books", books.size());
         return books;
+    }
+
+    // Fetch a book by ID (ID is now of type long)
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable long id) {
+        logger.debug("Fetching book with ID: {}", id);
+
+        Optional<Book> book = bookRepository.findById(id);
+
+        if (book.isPresent()) {
+            logger.debug("Found book: {}", book.get());
+            return book.get(); // Return the found book
+        } else {
+            logger.error("Book not found with ID: {}", id);
+            throw new BookNotFoundException(id); // Custom exception for not found books
+        }
+    }
+}
+
+// Custom exception to handle book not found scenario
+@ResponseStatus(value = org.springframework.http.HttpStatus.NOT_FOUND)
+class BookNotFoundException extends RuntimeException {
+    public BookNotFoundException(long id) {
+        super("Book not found with ID: " + id);
     }
 }
